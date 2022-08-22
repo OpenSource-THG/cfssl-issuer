e6c403af11975ebfc1fa16325f3954748973c232
Jun Eeo <jun.eeo@thehutgroup.com>
2022-08-22 18:18:23 +0100

    add cert-manager CRDs for test API server

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -72,7 +72,10 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "config", "crd", "bases"),
+			filepath.Join("..", "config", "crd", "tests"),
+		},
 		ErrorIfCRDPathMissing: true,
 	}
 
